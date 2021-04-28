ed9149696da0f3880c5ab1b823b30b19bb1ed22b
Mwangikimathi <kimmwangi46@gmail.com>
2021-04-28 18:10:18 +0300

    changes made2

--- configs/base_config.py
+++ configs/base_config.py
@@ -12,11 +12,6 @@ class Development(Base):
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 class Staging(Base):
-    DATABASE = "df3sunbe9qgbrq"
-    POSTGRES_USER = "qtlsbipodudbfx"
-    POSTGRES_PASSWORD = "6a283e2624f3a2d08f7ddf67c1534378d04660edc7438830fb3817e8e6e7b8f1"
-    SQLALCHEMY_DATABASE_URI ="postgres://qtlsbipodudbfx:6a283e2624f3a2d08f7ddf67c1534378d04660edc7438830fb3817e8e6e7b8f1@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/df3sunbe9qgbrq"
-    pass
 
 class Production(Base):
     pass
